950a52362cb407524ad3e26c65f951af5426dbdf
Kiran Kumar Kotari <kirankotari@live.com>
2019-02-09 13:13:37 +0530

    added test cases

---
+++ test/test_uninstall.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+
+set PATH=%~dp0..\bin;%~dp0..\shims;%PATH%
+
+echo :uninstall: test
+call pyenv versions
+call pyenv uninstall 3.5.2
+call pyenv uninstall 2.7.15
+call pyenv uninstall 3.7.2
+
