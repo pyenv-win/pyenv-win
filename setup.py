e24e62f455aaed8ec3fc35a0f423cb749b72761a
Kiran Kumar Kotari <kirankotari@live.com>
2019-02-11 23:30:33 +0530

    version update for pypi

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
 	name='pyenv-win',
-	version='1.1.1',
+	version='1.1.2',
 	description="pyenv lets you easily switch between multiple versions of Python. It's simple, unobtrusive, and follows the UNIX tradition of single-purpose tools that do one thing well.",
 	long_description = long_description,
 	long_description_content_type = 'text/markdown',
