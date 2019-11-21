a362d7744cf49c4d73d28ad46b7b131056b3c604
Kiran Kumar Kotari <kirankotari@live.com>
2019-11-21 21:14:26 +0530

    test-case: pyenv existing tests update

--- tests/test_pyenv.py
+++ tests/test_pyenv.py
@@ -14,21 +14,32 @@ class TestPyenv:
         sys.path.append(str(shims_path))
         yield bin_path
 
-    def test_pyenv_env_path(self, setup):
+    def test_check_pyenv_path(self, setup):
         assert setup.exists() is True
         assert str(setup) in sys.path
     
-    # def test_pyenv_version(self, setup):
-    #     ver_path = str(setup.parent.parent / '.version')
-    #     version = open(ver_path).read()
-    #     result = subprocess.run(['pyenv', '--version'], capture_output=True, shell=True)
-    #     print(":: Result :: {}".format(result))
-    #     print(":: Version :: {}".format(version))
-    #     assert version in str(result.stdout, "utf-8")
+    def test_check_pyenv_version(self, setup):
+        ver_path = str(setup.parent.parent / '.version')
+        version = open(ver_path).read().strip()
+        result = subprocess.run(['pyenv'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(":: Result :: {}".format(result))
+        print(":: Version :: {}".format(version))
+        assert version in str(result.stdout, "utf-8")
     
-    # def test_pyenv_features(self, setup):
-    #     result = subprocess.run(['pyenv'], capture_output=True, shell=True)
-    #     print(":: Result :: {}".format(result))
-    #     result = str(result.stdout, "utf-8")
-    #     assert 'install' in result
-    # pass
+    def test_check_pyenv_features_list(self, setup):
+        result = subprocess.run(['pyenv'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(":: Result :: {}".format(result))
+        result = str(result.stdout, "utf-8")
+        assert 'commands' in result
+        assert 'duplicate' in result
+        assert 'local' in result
+        assert 'global' in result
+        assert 'shell' in result
+        assert 'install' in result
+        assert 'uninstall' in result
+        assert 'rehash' in result
+        assert 'version' in result
+        assert 'versions' in result
+        assert 'exec' in result
+        assert 'which' in result
+        assert 'whence' in result
