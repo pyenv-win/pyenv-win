a82758ebe5a63c317b1f6ab7587de8617b0be531
dedale <ded@laposte.net>
2021-03-22 14:31:55 +0100

    Refactor pyenv local tests

--- tests/test_pyenv_feature_local.py
+++ tests/test_pyenv_feature_local.py
@@ -1,38 +1,29 @@
-import subprocess
-import tempfile
-from pathlib import Path
 from test_pyenv import TestPyenvBase
-from test_pyenv_helpers import install_pyenv, temp_pyenv, working_directory
+from test_pyenv_helpers import not_installed_output, run_pyenv_test
 
 
 class TestPyenvFeatureLocal(TestPyenvBase):
     def test_no_local_version(self, setup):
-        with temp_pyenv("local") as output:
-            assert output == "no local version configured for this directory"
+        def commands(ctx):
+            assert ctx.pyenv("local") == "no local version configured for this directory"
+        run_pyenv_test({}, commands)
 
     def test_local_version_defined(self, setup):
-        with temp_pyenv("local", local_ver="3.8.9") as output:
-            assert output == "3.8.9"
+        def commands(ctx):
+            assert ctx.pyenv("local") == "3.8.9"
+        run_pyenv_test({'local_ver': "3.8.9"}, commands)
 
     def test_local_set_installed_version(self, setup):
-        with tempfile.TemporaryDirectory() as tmp_path:
-            install_pyenv(tmp_path, ["3.7.7", "3.8.9"], local_ver="3.8.9")
-            with working_directory(tmp_path):
-                bat = Path(tmp_path, r'bin\pyenv.bat')
-
-                def pyenv_local(option=None):
-                    args = ['cmd', '/d', '/c', f'call {bat}', "local"]
-                    if option is not None:
-                        args.append(option)
-                    result = subprocess.run(args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                    output = str(result.stdout, "utf-8").strip()
-                    return output
-
-                assert pyenv_local("3.7.7") == ""
-                assert pyenv_local() == "3.7.7"
+        def commands(ctx):
+            assert ctx.pyenv(["local", "3.7.7"]) == ""
+            assert ctx.pyenv("local") == "3.7.7"
+        settings = {
+            'versions': ["3.7.7", "3.8.9"],
+            'local_ver': "3.8.9",
+        }
+        run_pyenv_test(settings, commands)
 
     def test_local_set_unknown_version(self, setup):
-        with temp_pyenv("local", "3.7.8", ["3.8.9"]) as output:
-            assert output == ("pyenv specific python requisite didn't meet. "
-                              "Project is using different version of python.\r\n"
-                              "Install python '3.7.8' by typing: 'pyenv install 3.7.8'")
+        def commands(ctx):
+            assert ctx.pyenv(["local", "3.7.8"]) == not_installed_output("3.7.8")
+        run_pyenv_test({'versions': ["3.8.9"]}, commands)
--- tests/test_pyenv_helpers.py
+++ tests/test_pyenv_helpers.py
@@ -34,6 +34,54 @@ def script_exes(ver):
         yield f'easy_install{suffix}.exe'
 
 
+def pyenv_setup(settings):
+    pyenv_path, local_path, versions, global_ver, local_ver =\
+        settings['pyenv_path'],\
+        settings['local_path'],\
+        settings.get('versions', None),\
+        settings.get("global_ver", None),\
+        settings.get('local_ver', None)
+    if versions is None:
+        versions = []
+    src_path = Path(__file__).resolve().parents[1].joinpath('pyenv-win')
+    dirs = [r'bin', r'libexec\libs', r'shims', r'versions']
+    for d in dirs:
+        os.makedirs(Path(pyenv_path, d))
+    files = [r'bin\pyenv.bat',
+             r'libexec\pyenv.vbs',
+             r'libexec\libs\pyenv-install-lib.vbs',
+             r'libexec\libs\pyenv-lib.vbs']
+    for f in files:
+        shutil.copy(src_path.joinpath(f), Path(pyenv_path, f))
+    versions_dir = Path(pyenv_path, r'versions')
+
+    def touch(exe):
+        with open(exe, 'a'):
+            os.utime(exe, None)
+
+    def create_pythons(path):
+        os.mkdir(path)
+        for exe in python_exes([f'{ver.major}', f'{ver.major}{ver.minor}']):
+            touch(path.joinpath(exe))
+        return path
+
+    def create_scripts(path):
+        os.mkdir(path)
+        for exe in script_exes(ver):
+            touch(path.joinpath(exe))
+
+    for v in versions:
+        ver = version.parse(v)
+        version_path = create_pythons(versions_dir.joinpath(v))
+        create_scripts(version_path.joinpath('Scripts'))
+    if global_ver is not None:
+        with open(Path(pyenv_path, "version"), "w") as f:
+            print(global_ver, file=f)
+    if local_ver is not None:
+        with open(Path(local_path, ".python-version"), "w") as f:
+            print(local_ver, file=f)
+
+
 def install_pyenv(root_path, versions=None, global_ver=None, local_ver=None):
     if versions is None:
         versions = []
@@ -90,3 +138,49 @@ def temp_pyenv(command, option=None, versions=None, global_ver=None, local_ver=N
             result = subprocess.run(args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             result = str(result.stdout, "utf-8").strip()
             yield result
+
+
+class PyenvContext:
+    def __init__(self, pyenv, pyenv_path, local_path):
+        self.pyenv = pyenv
+        self.pyenv_path = pyenv_path
+        self.local_path = local_path
+
+
+def run_pyenv_test(settings, commands):
+    with tempfile.TemporaryDirectory() as tmp_path:
+        settings['pyenv_path'] = pyenv_path = Path(tmp_path, 'pyenv')
+        settings['local_path'] = local_path = Path(tmp_path, 'local')
+        os.mkdir(pyenv_path)
+        os.mkdir(local_path)
+        pyenv_setup(settings)
+        with working_directory(local_path):
+            bat = Path(pyenv_path, r'bin\pyenv.bat')
+
+            def pyenv(pyenv_args=None):
+                args = ['cmd', '/d', '/c', f'call {bat}']
+                if pyenv_args is not None:
+                    if isinstance(pyenv_args, list):
+                        args = args + pyenv_args
+                    else:
+                        args.append(pyenv_args)
+                result = subprocess.run(args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                stderr = str(result.stderr, "utf-8").strip()
+                if stderr != "":
+                    print(stderr)
+                stdout = str(result.stdout, "utf-8").strip()
+                return stdout
+
+            context = PyenvContext(pyenv, pyenv_path, local_path)
+            commands(context)
+
+
+def not_installed_output(ver):
+    return (f"pyenv specific python requisite didn't meet. "
+            f"Project is using different version of python.\r\n"
+            f"Install python '{ver}' by typing: 'pyenv install {ver}'")
+
+
+def local_python_versions(path):
+    with open(Path(path, '.python-version'), mode='r') as f:
+        return f.read().strip()
