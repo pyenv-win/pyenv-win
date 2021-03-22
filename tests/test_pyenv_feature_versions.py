aa95ff1ba621b46ab0f90fbc6dc524ba7053274c
dedale <ded@laposte.net>
2021-03-22 19:43:58 +0100

    Add pyenv version tests

--- tests/test_pyenv_feature_versions.py
+++ tests/test_pyenv_feature_versions.py
@@ -1,3 +1,4 @@
+from tempenv import TemporaryEnvironment
 from test_pyenv import TestPyenvBase
 from test_pyenv_helpers import run_pyenv_test
 
@@ -15,3 +16,75 @@ class TestPyenvFeatureVersions(TestPyenvBase):
                 assert v in output
         versions = ['3.7.4', '3.8.5']
         run_pyenv_test({'versions': versions}, commands)
+
+    def test_list_current_global_version(self, setup):
+        def commands(ctx):
+            assert ctx.pyenv("versions") == (f"  3.6.5\r\n"
+                                             f"* 3.7.7 (set by {ctx.pyenv_path}\\version)\r\n"
+                                             f"  3.9.1")
+        settings = {
+            'versions': ["3.6.5", "3.7.7", "3.9.1"],
+            'global_ver': "3.7.7"
+        }
+        run_pyenv_test(settings, commands)
+
+    def test_list_current_local_version(self, setup):
+        def commands(ctx):
+            assert ctx.pyenv("versions") == (f"* 3.6.5 (set by {ctx.local_path}\\.python-version)\r\n"
+                                             f"  3.7.7\r\n"
+                                             f"  3.9.1")
+        settings = {
+            'versions': ["3.6.5", "3.7.7", "3.9.1"],
+            'global_ver': "3.7.7",
+            'local_ver': "3.6.5"
+        }
+        run_pyenv_test(settings, commands)
+
+    def test_list_current_shell_version(self, setup):
+        def commands(ctx):
+            assert ctx.pyenv("versions") == (f"  3.6.5\r\n"
+                                             f"  3.7.7\r\n"
+                                             f"* 3.9.1 (set by %PYENV_VERSION%)")
+        settings = {
+            'versions': ["3.6.5", "3.7.7", "3.9.1"],
+            'global_ver': "3.7.7",
+            'local_ver': "3.6.5"
+        }
+        with TemporaryEnvironment({"PYENV_VERSION": "3.9.1"}):
+            run_pyenv_test(settings, commands)
+
+    def test_list_uninstalled_current_global_version(self, setup):
+        def commands(ctx):
+            assert ctx.pyenv("versions") == (f"  3.6.5\r\n"
+                                             f"  3.7.7\r\n"
+                                             f"  3.9.1")
+        settings = {
+            'versions': ["3.6.5", "3.7.7", "3.9.1"],
+            'global_ver': "3.7.5"
+        }
+        run_pyenv_test(settings, commands)
+
+    def test_list_uninstalled_local_version(self, setup):
+        def commands(ctx):
+            assert ctx.pyenv("versions") == (f"  3.6.5\r\n"
+                                             f"  3.7.7\r\n"
+                                             f"  3.9.1")
+        settings = {
+            'versions': ["3.6.5", "3.7.7", "3.9.1"],
+            'global_ver': "3.7.7",
+            'local_ver': "3.6.1"
+        }
+        run_pyenv_test(settings, commands)
+
+    def test_list_uninstalled_shell_version(self, setup):
+        def commands(ctx):
+            assert ctx.pyenv("versions") == (f"  3.6.5\r\n"
+                                             f"  3.7.7\r\n"
+                                             f"  3.9.1")
+        settings = {
+            'versions': ["3.6.5", "3.7.7", "3.9.1"],
+            'global_ver': "3.7.7",
+            'local_ver': "3.6.5"
+        }
+        with TemporaryEnvironment({"PYENV_VERSION": "3.9.2"}):
+            run_pyenv_test(settings, commands)
