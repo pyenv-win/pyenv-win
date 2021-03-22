b7f08cb20de93108a3a468eed4a274f80a9b83af
dedale <ded@laposte.net>
2021-03-22 17:15:26 +0100

    Refactor pyenv versions tests

--- tests/test_pyenv_feature_versions.py
+++ tests/test_pyenv_feature_versions.py
@@ -1,14 +1,17 @@
 from test_pyenv import TestPyenvBase
-from test_pyenv_helpers import temp_pyenv
+from test_pyenv_helpers import run_pyenv_test
 
 
 class TestPyenvFeatureVersions(TestPyenvBase):
     def test_list_no_version(self, setup):
-        with temp_pyenv("versions") as output:
-            assert output == ""
+        def commands(ctx):
+            assert ctx.pyenv("versions") == ""
+        run_pyenv_test({}, commands)
 
     def test_list_all_versions(self, setup):
-        versions = ['3.7.4', '3.8.5']
-        with temp_pyenv("versions", versions=versions) as output:
+        def commands(ctx):
+            output = ctx.pyenv("versions")
             for v in versions:
                 assert v in output
+        versions = ['3.7.4', '3.8.5']
+        run_pyenv_test({'versions': versions}, commands)
