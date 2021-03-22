351a43f6aef93bd7f970b715e6c13197bf930eb6
dedale <ded@laposte.net>
2021-03-22 17:17:15 +0100

    Remove unused test code

--- tests/test_pyenv_helpers.py
+++ tests/test_pyenv_helpers.py
@@ -82,64 +82,6 @@ def pyenv_setup(settings):
             print(local_ver, file=f)
 
 
-def install_pyenv(root_path, versions=None, global_ver=None, local_ver=None):
-    if versions is None:
-        versions = []
-    src_path = Path(__file__).resolve().parents[1].joinpath('pyenv-win')
-    dirs = [r'bin', r'libexec\libs', r'shims', r'versions']
-    for d in dirs:
-        os.makedirs(Path(root_path, d))
-    files = [r'bin\pyenv.bat',
-             r'libexec\pyenv.vbs',
-             r'libexec\libs\pyenv-install-lib.vbs',
-             r'libexec\libs\pyenv-lib.vbs']
-    for f in files:
-        shutil.copy(src_path.joinpath(f), Path(root_path, f))
-    versions_dir = Path(root_path, r'versions')
-
-    def touch(exe):
-        with open(exe, 'a'):
-            os.utime(exe, None)
-
-    def create_pythons(path):
-        os.mkdir(path)
-        for exe in python_exes([f'{ver.major}', f'{ver.major}{ver.minor}']):
-            touch(path.joinpath(exe))
-        return path
-
-    def create_scripts(path):
-        os.mkdir(path)
-        for exe in script_exes(ver):
-            touch(path.joinpath(exe))
-
-    for v in versions:
-        ver = version.parse(v)
-        version_path = create_pythons(versions_dir.joinpath(v))
-        create_scripts(version_path.joinpath('Scripts'))
-    if global_ver is not None:
-        with open(Path(root_path, "version"), "w") as f:
-            print(global_ver, file=f)
-    if local_ver is not None:
-        with open(Path(root_path, ".python-version"), "w") as f:
-            print(local_ver, file=f)
-
-
-@contextmanager
-def temp_pyenv(command, option=None, versions=None, global_ver=None, local_ver=None):
-    if versions is None:
-        versions = []
-    with tempfile.TemporaryDirectory() as tmp_path:
-        install_pyenv(tmp_path, versions, global_ver, local_ver)
-        with working_directory(tmp_path):
-            pyenv_bat = Path(tmp_path, r'bin\pyenv.bat')
-            args = ['cmd', '/d', '/c', f'call {pyenv_bat}', command]
-            if option is not None:
-                args.append(option)
-            result = subprocess.run(args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            result = str(result.stdout, "utf-8").strip()
-            yield result
-
-
 class PyenvContext:
     def __init__(self, pyenv, pyenv_path, local_path):
         self.pyenv = pyenv
