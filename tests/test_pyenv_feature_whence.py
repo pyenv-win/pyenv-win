854291f2017c3211239a6377dbffca3d117e88c4
dedale <ded@laposte.net>
2021-03-22 23:27:27 +0100

    Add pyenv whence tests

--- tests/test_pyenv_feature_whence.py
+++ tests/test_pyenv_feature_whence.py
@@ -1,6 +1,22 @@
 from test_pyenv import TestPyenvBase
+from test_pyenv_helpers import run_pyenv_test
 
 
 class TestPyenvFeatureWhence(TestPyenvBase):
-    pass
+    def test_whence_major(self, setup):
+        def commands(ctx):
+            for name in ['python', 'python3', 'pip3']:
+                assert ctx.pyenv(["whence", name]) == '3.7.7\r\n3.8.2\r\n3.8.7\r\n3.9.1'
+        run_pyenv_test({'versions': ['3.7.7', '3.8.2', '3.8.7', '3.9.1']}, commands)
 
+    def test_whence_major_minor(self, setup):
+        def commands(ctx):
+            for name in ['python38', 'pip3.8']:
+                assert ctx.pyenv(["whence", name]) == '3.8.2\r\n3.8.7'
+        run_pyenv_test({'versions': ['3.7.7', '3.8.2', '3.8.7', '3.9.1']}, commands)
+
+    def test_whence_not_found(self, setup):
+        def commands(ctx):
+            for name in ['python3.8']:
+                assert ctx.pyenv(["whence", name]) == ""
+        run_pyenv_test({'versions': ['3.7.7', '3.8.2', '3.8.7', '3.9.1']}, commands)
