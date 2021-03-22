a403993a90b27f8123580cabacb783c170581368
dedale <ded@laposte.net>
2021-03-22 17:08:21 +0100

    Refactor pyenv rehash tests

--- tests/test_pyenv_feature_rehash.py
+++ tests/test_pyenv_feature_rehash.py
@@ -1,21 +1,8 @@
-import subprocess
-import tempfile
 from packaging import version
 from pathlib import Path
 from tempenv import TemporaryEnvironment
 from test_pyenv import TestPyenvBase
-from test_pyenv_helpers import install_pyenv, python_exes, script_exes, temp_pyenv, working_directory
-
-
-def set_local_version(path, ver):
-    with open(Path(path, ".python-version"), "w") as f:
-        print(ver, file=f)
-
-
-def pyenv_rehash(pyenv_path):
-    bat = Path(pyenv_path, r'bin\pyenv.bat')
-    args = ['cmd', '/d', '/c', f'call {bat}', 'rehash']
-    subprocess.run(args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+from test_pyenv_helpers import python_exes, script_exes, run_pyenv_test
 
 
 def assert_shims(pyenv_path, ver):
@@ -30,33 +17,38 @@ def assert_shims(pyenv_path, ver):
 
 class TestPyenvFeatureRehash(TestPyenvBase):
     def test_rehash_global_version(self, setup):
-        with tempfile.TemporaryDirectory() as pyenv_path:
-            install_pyenv(pyenv_path, versions=['3.8.6', '3.8.7'], global_ver='3.8.6')
-            with working_directory(pyenv_path):
-                pyenv_rehash(pyenv_path)
-                assert_shims(pyenv_path, '3.8.6')
-                assert_shims(pyenv_path, '3.8.7')
+        def commands(ctx):
+            assert ctx.pyenv('rehash') == ""
+            assert_shims(ctx.pyenv_path, '3.8.6')
+            assert_shims(ctx.pyenv_path, '3.8.7')
+        settings = {
+            'versions': ['3.8.6', '3.8.7'],
+            'global_ver': '3.8.6',
+        }
+        run_pyenv_test(settings, commands)
 
     def test_rehash_local_version(self, setup):
-        with tempfile.TemporaryDirectory() as pyenv_path, tempfile.TemporaryDirectory() as cur_path:
-            install_pyenv(pyenv_path, versions=['3.8.6', '3.9.1'], global_ver='3.8.6')
-            with working_directory(cur_path):
-                set_local_version(cur_path, '3.9.1')
-                pyenv_rehash(pyenv_path)
-                assert_shims(pyenv_path, '3.8.6')
-                assert_shims(pyenv_path, '3.9.1')
+        def commands(ctx):
+            assert ctx.pyenv('rehash') == ""
+            assert_shims(ctx.pyenv_path, '3.8.6')
+            assert_shims(ctx.pyenv_path, '3.9.1')
+        settings = {
+            'versions': ['3.8.6', '3.9.1'],
+            'global_ver': '3.8.6',
+            'local_ver': '3.9.1'
+        }
+        run_pyenv_test(settings, commands)
 
     def test_rehash_shell_version(self, setup):
-        with tempfile.TemporaryDirectory() as pyenv_path, tempfile.TemporaryDirectory() as cur_path:
-            global_ver = '3.7.5'
-            local_ver = '3.8.6'
-            shell_ver = '3.9.1'
-            install_pyenv(pyenv_path, versions=[global_ver, local_ver, shell_ver], global_ver=global_ver)
-            with working_directory(cur_path):
-                with TemporaryEnvironment({"PYENV_VERSION": shell_ver}):
-                    set_local_version(cur_path, local_ver)
-                    pyenv_rehash(pyenv_path)
-                    assert_shims(pyenv_path, global_ver)
-                    assert_shims(pyenv_path, local_ver)
-                    assert_shims(pyenv_path, shell_ver)
-
+        def commands(ctx):
+            assert ctx.pyenv('rehash') == ""
+        global_ver = '3.7.5'
+        local_ver = '3.8.6'
+        shell_ver = '3.9.1'
+        settings = {
+            'versions': [global_ver, local_ver, shell_ver],
+            'global_ver': global_ver,
+            'local_ver': local_ver
+        }
+        with TemporaryEnvironment({"PYENV_VERSION": shell_ver}):
+            run_pyenv_test(settings, commands)
