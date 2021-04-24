f2e40b4267b094b4be86478189bf07560d171e14
papercuptech <paul@thefamilyroom.biz>
2021-04-24 12:27:48 -0400

    fix exec
    ensure some version is active before running 'exec'
    reset local vars to support pyenv.bat reentry
    special case '.cmd' vs '.bat' plugins
      '.bat' can alter shell vars, but PATH not set to active version
      '.cmd' (and '.vbs') just the opposite

--- pyenv-win/bin/pyenv.bat
+++ pyenv-win/bin/pyenv.bat
@@ -11,6 +11,7 @@ if [%1]==[] (
 )
 
 rem use pyenv.vbs to aid resolving absolute path of "active" version into 'bindir'
+set "bindir="
 for /f %%i in ('%pyenv% version') do call :normalizepath "%~dp0..\versions\%%i" bindir
 
 rem all help implemented as plugin
@@ -32,6 +33,11 @@ if /i not [%1]==[exec] goto :plugin
 rem ====================================================================================
 :exec
 
+echo %bindir%
+if not exist "%bindir%" (
+	echo No global python version has been set yet. Please set the global version.
+)
+
 shift
 
 for /f "tokens=1,2 delims=/" %%i in ("%1") do set "exepath=%%i" & set "exe=%%j"
@@ -50,6 +56,9 @@ call :normalizepath %exe% exe
 
 if exist "%exe%.bat" (
   set "exe=endlocal & call "%exe%.bat""
+
+) else if exist "%exe.cmd%" (
+	set "exe=call "%exe%.cmd""
 
 ) else if exist "%exe%.vbs" (
   set "exe=cscript //nologo "%exe%.vbs""
@@ -70,6 +79,7 @@ set "path=%bindir%;%bindir%\Scripts;%path%"
 rem copy params to program precisely, preserving double-quotes and percents.
 rem this is the main fix for how 'exec' processing in pyenv.vbs did not
 rem correctly handle (get passed) params with percent chars in them
+set "params="
 :paramloop
 if not [%2]==[] (
   set params=%params% %2
