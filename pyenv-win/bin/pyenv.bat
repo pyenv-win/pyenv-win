b9646c0cd109d79135102c7383e1afe09f602483
dedale <ded@laposte.net>
2021-05-01 09:38:17 +0200

    Refactor pyenv.bat

--- pyenv-win/bin/pyenv.bat
+++ pyenv-win/bin/pyenv.bat
@@ -4,17 +4,17 @@ chcp 1250 >nul
 
 set "pyenv=cscript //nologo "%~dp0..\libexec\pyenv.vbs""
 
-rem if 'pyenv' called alone, then run pyenv.vbs
+:: if 'pyenv' called alone, then run pyenv.vbs
 if [%1]==[] (
   %pyenv%
   exit /b
 )
 
-rem use pyenv.vbs to aid resolving absolute path of "active" version into 'bindir'
+:: use pyenv.vbs to aid resolving absolute path of "active" version into 'bindir'
 set "bindir="
 for /f %%i in ('%pyenv% version') do call :normalizepath "%~dp0..\versions\%%i" bindir
 
-rem all help implemented as plugin
+:: all help implemented as plugin
 if /i [%2]==[--help] goto :plugin
 if /i [%1]==[--help] (
   call :plugin %2 %1
@@ -25,7 +25,7 @@ if /i [%1]==[help] (
   exit /b
 )
 
-rem let pyenv.vbs handle these
+:: let pyenv.vbs handle these
 set commands=rehash global local version vname version-name versions commands shims which whence help --help
 for %%a in (%commands%) do (
   if /i [%1]==[%%a] (
@@ -34,11 +34,10 @@ for %%a in (%commands%) do (
   )
 )
 
-rem jump to plugin or fall to exec
+:: jump to plugin or fall to exec
 if /i not [%1]==[exec] goto :plugin
 
-
-rem ====================================================================================
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :exec
 
 if not exist "%bindir%" (
@@ -52,11 +51,7 @@ for /f "tokens=1,2 delims=/" %%i in ("%1") do set "exepath=%%i" & set "exe=%%j"
 if [%exe%]==[] (set "exe=%exepath%") else (set "exe=%bindir%\%exepath%\%exe%")
 set "exe="%exe%""
 goto :run
-
-
-
-
-rem ====================================================================================
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :plugin
 
 set "exe=%~dp0..\libexec\pyenv-%1"
@@ -75,18 +70,14 @@ if exist "%exe%.bat" (
   echo pyenv: no such command '%1'
   exit /b
 )
-
-
-
-rem ====================================================================================
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :run
-
-rem update PATH to active version
+:: update PATH to active version
 set "path=%bindir%;%bindir%\Scripts;%path%"
 
-rem copy params to program precisely, preserving double-quotes and percents.
-rem this is the main fix for how 'exec' processing in pyenv.vbs did not
-rem correctly handle (get passed) params with percent chars in them
+:: copy params to program precisely, preserving double-quotes and percents.
+:: this is the main fix for how 'exec' processing in pyenv.vbs did not
+:: correctly handle (get passed) params with percent chars in them
 set "params="
 :paramloop
 if not [%2]==[] (
@@ -95,16 +86,12 @@ if not [%2]==[] (
   goto paramloop
 )
 
-rem run exec or plugin
+:: run exec or plugin
 %exe% %params%
 exit /b
-
-
-
-
-rem ====================================================================================
-rem convert path which may have relative nodes (.. or .)
-rem to its absolute value so can be used in PATH
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: convert path which may have relative nodes (.. or .)
+:: to its absolute value so can be used in PATH
 :normalizepath
 set "%~2=%~dpf1"
 exit /b
