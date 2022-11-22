da1d77ab7137ad16f56c64167eb6b23caf48ca17
Kiran Kumar Kotari <kirankotari@live.com>
2022-11-22 09:32:40 -0500

    updating docs

--- tests/test_pyenv_feature_version.py
+++ tests/test_pyenv_feature_version.py
@@ -24,7 +24,9 @@ def test_no_version(pyenv):
         (
             "No global/local python version has been set yet. "
             "Please set the global/local version by typing:\r\n"
+            "pyenv global <python-version>\r\n"
             "pyenv global 3.7.4\r\n"
+            "pyenv local <python-version>\r\n"
             "pyenv local 3.7.4"
         ),
         ""
